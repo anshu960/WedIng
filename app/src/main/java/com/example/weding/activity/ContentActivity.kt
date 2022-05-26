a15640dfeecf07cb7cf15539aceef591ecaa0059
Anshu <anshukumarsingh448@gmail.com>
2022-05-26 15:09:34 +0530

    SQLite Database2

--- app/src/main/java/com/example/weding/activity/ContentActivity.kt
+++ app/src/main/java/com/example/weding/activity/ContentActivity.kt
@@ -1,8 +1,6 @@
 package com.example.weding.activity
 
 
-import android.Manifest
-import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
@@ -12,18 +10,17 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.weding.R
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_content.*
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class ContentActivity : AppCompatActivity(), View.OnClickListener {
     private var cal = Calendar.getInstance()
@@ -35,8 +32,7 @@ class ContentActivity : AppCompatActivity(), View.OnClickListener {
         toolBar.setNavigationOnClickListener {
             onBackPressed()
         }
-        dateSetListener = DatePickerDialog.OnDateSetListener {
-                _, year, month, dayOfMonth ->
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
             cal.set(Calendar.YEAR, year)
             cal.set(Calendar.MONTH, month)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
@@ -44,97 +40,88 @@ class ContentActivity : AppCompatActivity(), View.OnClickListener {
         }
         date.setOnClickListener(this)
         tv_add_image.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id){
-            R.id.date ->{
-                DatePickerDialog(this@ContentActivity, dateSetListener,
+        when (v!!.id) {
+            R.id.date -> {
+                DatePickerDialog(
+                    this@ContentActivity, dateSetListener,
                     cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)).show()
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
             }
-            R.id.tv_add_image ->{
-                val pictureDialog = AlertDialog.Builder(this)
-                pictureDialog.setTitle("Select Action")
-                val pictureDialogItems = arrayOf("Select photo from Gallery",
-                                                 "Capture photo from Camera")
-                pictureDialog.setItems(pictureDialogItems){
-                        _, which ->
-                    when(which){
-                        0 -> choosePhotoFromGallery()
-                        1 -> Toast.makeText(
-                        this@ContentActivity,
-                        "Camera selection",
-                        Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-                pictureDialog.show()
-            }
+             R.id.tv_add_image -> {
+                 val pictureDialog = AlertDialog.Builder(this)
+                 pictureDialog.setTitle("Select Action")
+                 val pictureDialogItems = arrayOf("Select photo from Gallery","Capture photo from Camera")
+                 pictureDialog.setItems(pictureDialogItems){
+                         _, which ->
+                     when(which){
+                         0 -> choosePhotoFromGallery()
+                             1 -> Toast.makeText(this@ContentActivity,
+                         "Camera selection coming soon...",
+                         Toast.LENGTH_SHORT).show()
+                     }
+                 }
+                 pictureDialog.show()
+             }
         }
     }
+private fun choosePhotoFromGallery(){
+    Dexter.withActivity(this).withPermissions(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ).withListener(object: MultiplePermissionsListener{
+        override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
 
-    private fun choosePhotoFromGallery() {
-        Dexter.withActivity(this).withPermission(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ).withListener(object: MultiplePermissionsListener, PermissionListener {
-            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                if (report != null) {
-                    if (report.areAllPermissionsGranted()){
-                        Toast.makeText(this@ContentActivity,
-                        "Storage READ/WRITE permission are granted", Toast.LENGTH_SHORT).show()
-                    }
-
+                if (report!!.areAllPermissionsGranted()){
+                    Toast.makeText(this@ContentActivity,
+                        "Storage READ/WRITE permission are granted. Now you can select an image from GALLERY",
+                        Toast.LENGTH_SHORT)
+                        .show()
                 }
-            }
-
-            override fun onPermissionRationaleShouldBeShown(
-                permissions: MutableList<PermissionRequest>?,
-                token: PermissionToken?
-            ) {
-
-            }
-
-            override fun onPermissionGranted(response: PermissionGrantedResponse?) {
-
-            }
-
-            override fun onPermissionDenied(response: PermissionDeniedResponse?) {
 
             }
 
 
-            override fun onPermissionRationaleShouldBeShown(
-                permission: PermissionRequest?,
-                token: PermissionToken?
-            ) {
+        override fun onPermissionRationaleShouldBeShown(
+            permissions: MutableList<PermissionRequest>?,
+            token: PermissionToken?
+        ) {
+           showRationalDialogForPermission()
+        }
 
-                AlertDialog.Builder(this@ContentActivity).setMessage("It looks like you have turned off permission required for this feature. It can be enabled under the Application Setting"
-                ).setPositiveButton("Go to SETTINGS")
-                {_, _ ->
-                    try {
-                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                        val uri = Uri.fromParts("package", packageName, null)
-                        intent.data = uri
-                        startActivity(intent)
-                    } catch (e: ActivityNotFoundException){
-                        e.printStackTrace()
-                    }
+    }).onSameThread().check()
+}
 
-                }.setNegativeButton("Cancel"){ dialog, _ ->
-                    dialog.dismiss()
-                }.show()
-            }
-        }).onSameThread().check()
-
+    private fun showRationalDialogForPermission() {
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off permission required for this feature. I t can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS")
+            {
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val  uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel"){
+                    dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
-    private fun updateDateInView(){
+    private fun updateDateInView() {
         val myFormat = "dd.MM.yyyy"
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
         date.setText(sdf.format(cal.time).toString())
     }
 }
+
 
 
