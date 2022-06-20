f67646d1ad29b392f642a2d9198180829bbda3ce
Anshu <anshukumarsingh448@gmail.com>
2022-06-20 20:50:05 +0530

    final SqLite

--- app/src/main/java/com/example/weding/activity/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/weding/activity/Activity/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.example.weding.activity.Activity
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import com.example.weding.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,6 +23,10 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var progressDialog: ProgressDialog
 
     //firebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -26,6 +35,66 @@ class LoginActivity : AppCompatActivity() {
         actionBar = supportActionBar!!
         actionBar.title = "Login"
 
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging in...")
+        progressDialog.setCanceledOnTouchOutside(false)
 
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click open SignUpActivity
+        binding.noAccount.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        //handle click login
+        binding.signin.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+        email = binding.emailEt.text.toString().trim()
+        password = binding.textpassword.text.toString().trim()
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+           //invalid email format
+            binding.emailEt.error = "Invalid email format"
+        }
+        else if (TextUtils.isEmpty(password)){
+            binding.textpassword.error = "Please enter password"
+        }
+        else{
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+              progressDialog.dismiss()
+              val firebaseUser = firebaseAuth.currentUser
+              val email = firebaseUser!!.email
+              Toast.makeText(this, "LoggedIn as $email", Toast.LENGTH_SHORT).show()
+              startActivity(Intent(this, WedinActivity::class.java))
+              finish()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            startActivity(Intent(this, WedinActivity::class.java))
+            finish()
+        }
     }
 }
