8889b003d404adc84754c02aa302dee71b586199
Anshu <anshukumarsingh448@gmail.com>
2022-07-05 14:45:50 +0530

    final update

--- app/src/main/java/com/example/weding/activity/Activity/WedinActivity.kt
+++ app/src/main/java/com/example/weding/activity/Activity/WedinActivity.kt
@@ -1,12 +1,15 @@
 package com.example.weding.activity.Activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.weding.R
 import com.example.weding.activity.database.DatabaseHandler
 import com.example.weding.activity.model.HappyPlaceModel
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_wedin.*
 
 
@@ -17,6 +20,10 @@ class WedinActivity : AppCompatActivity() {
         setContentView(R.layout.activity_wedin)
 
         supportActionBar?.hide()
+        btn_scan.setOnClickListener {
+            val scanner = IntentIntegrator(this)
+            scanner.initiateScan()
+        }
         Add_btn.setOnClickListener {
             val intent = Intent(this, ContentActivity::class.java)
             startActivity(intent)
@@ -32,6 +39,21 @@ class WedinActivity : AppCompatActivity() {
                 Log.e("Description", i.description)
             }
         }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+            if (result != null){
+                if (result.contents==null){
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_SHORT).show()
+                }
+            } else{
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+
     }
 }
 
