a9cd4a026d270d3705324789361a868d4e0d3b4c
Anshu <anshukumarsingh448@gmail.com>
2022-07-12 17:08:14 +0530

    final update

---
+++ app/src/main/java/com/example/weding/activity/Activity/WedinDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weding.activity.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weding.R
+
+class WedinDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wedin_detail)
+    }
+}
