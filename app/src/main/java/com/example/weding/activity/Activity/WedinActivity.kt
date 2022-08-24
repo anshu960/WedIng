225664ff2b37fbf4ce4e2370c27ffc499c17f291
Anshu <anshukumarsingh448@gmail.com>
2022-08-24 18:41:59 +0530

    My First Project

--- app/src/main/java/com/example/weding/activity/Activity/WedinActivity.kt
+++ app/src/main/java/com/example/weding/activity/Activity/WedinActivity.kt
@@ -1,9 +1,18 @@
 package com.example.weding.activity.Activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weding.R
+import com.example.weding.activity.Activity.Database.DatabaseHandler
+import com.example.weding.activity.Activity.adapter.HappyPlacesAdapter
+import com.example.weding.activity.Activity.model.HappyPlaceModel
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_wedin.*
 
 class WedinActivity : AppCompatActivity() {
@@ -13,8 +22,66 @@ class WedinActivity : AppCompatActivity() {
         supportActionBar?.hide()
         Add_btn.setOnClickListener {
             val intent = Intent(this, ContentActivity::class.java)
-            startActivity(intent)
-            //my new update
+            startActivityForResult(intent, ADD_PLACE_ACTIVITY_REQUEST_CODE)
+
+        }
+        getHappyPlacesListFromLocalDB()
+    }
+    private fun setupHappyPlacesRecyclerview(happyPlaceList: ArrayList<HappyPlaceModel>){
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        rv_happy_places_list.adapter = placesAdapter
+
+        placesAdapter.setOnClickListener(object : HappyPlacesAdapter.OnclickListener{
+            override fun onClick(position: Int, model: HappyPlaceModel) {
+                val intent = Intent(this@WedinActivity,
+                    HappyPlaceDetailActivity::class.java)
+                intent.putExtra(EXTRA_PLACE_DETAILS, model)
+                startActivity(intent)
+            }
+        })
+
+    }
+
+    private fun getHappyPlacesListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+        if (getHappyPlaceList.size > 0){
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecyclerview(getHappyPlaceList)
+        }else{
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
         }
     }
-}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == ADD_PLACE_ACTIVITY_REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                getHappyPlacesListFromLocalDB()
+            }else{
+                Log.e("Activity", "Cancelled or Back pressed")
+            }
+        }
+        if (resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+            if (result != null){
+                if (result.contents==null){
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_SHORT).show()
+                }
+            } else{
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+
+    }
+
+    companion object{
+        var ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
+        var EXTRA_PLACE_DETAILS = "extra_place_details"
+    }
+    }
